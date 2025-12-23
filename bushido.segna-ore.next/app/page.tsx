baade9595d4f57bbf2f3db5bbece3d98a83f46ba
Tocco17 <fede.ruocco@gmail.com>
2025-12-23 16:08:06 +0100

    layout

--- bushido.segna-ore.next/app/page.tsx
+++ bushido.segna-ore.next/app/page.tsx
@@ -44,28 +44,26 @@ export default async function Home() {
 	])
 
 	return (
-		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-				<DashboardCard
-					title="Lessons"
-					subtitle="Manage worked lessons"
-					link={"/lessons"}
-				>
-					<p>Total worked hours: {workedLessons}</p>
-				</DashboardCard>
+		<>
+			<DashboardCard
+				title="Lessons"
+				subtitle="Manage worked lessons"
+				link={"/lessons"}
+			>
+				<p>Total worked hours: {workedLessons}</p>
+			</DashboardCard>
 
-				<DashboardCard
-					title="Payments"
-					subtitle="Check payments received and to be received"
-					link={"/payments"}
-				>
-					<>
-						<p>Total payments to receive: {paymentToReceive} €</p>
-						<p>Total payments received: {paymentReceived} €</p>
-					</>
-				</DashboardCard>
-			</main>
-		</div>
+			<DashboardCard
+				title="Payments"
+				subtitle="Check payments received and to be received"
+				link={"/payments"}
+			>
+				<>
+					<p>Total payments to receive: {paymentToReceive} €</p>
+					<p>Total payments received: {paymentReceived} €</p>
+				</>
+			</DashboardCard>
+		</>
 	);
 }
 
