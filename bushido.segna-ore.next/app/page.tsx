6cc2e43b18be710fef678236f9ba0ea0f2b420ec
Tocco17 <fede.ruocco@gmail.com>
2025-12-23 11:38:47 +0100

    home page

--- bushido.segna-ore.next/app/page.tsx
+++ bushido.segna-ore.next/app/page.tsx
@@ -1,65 +1,90 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { antonio, antonioLessons } from "@/utils/db/antonio";
+import { productMoney } from "@/utils/operations/money-operations";
+import { JSX } from "react";
 
-export default function Home() {
+async function getWorkedLessonHours(): Promise<number> {
+	const workedLesson = antonioLessons
+	const totalHours = workedLesson
+		.reduce((previousHours, currentLesson) => previousHours + currentLesson.hours, 0)
+
+	return totalHours
+}
+
+async function getPaymentToReceive(): Promise<Money> {
+	const { paymentPerHour } = antonio
+	const notPayedLessons = antonioLessons.filter(lesson => !lesson.isItPaid)
+	const notPayedHours = notPayedLessons.reduce((previousHours, currentLesson) => previousHours + currentLesson.hours, 0)
+	const result = productMoney(paymentPerHour, notPayedHours)
+	return result
+}
+
+async function getPaymentReceived(): Promise<Money> {
+	const { paymentPerHour } = antonio
+	const payedLessons = antonioLessons.filter(lesson => lesson.isItPaid)
+	const payedHours = payedLessons.reduce((previousHours, currentLesson) => previousHours + currentLesson.hours, 0)
+	const result = productMoney(paymentPerHour, payedHours)
+	return result
+}
+
+
+
+export default async function Home() {
+	const [
+		workedLessons,
+		paymentToReceive,
+		paymentReceived,
+	] = await Promise.all([
+		getWorkedLessonHours(),
+		getPaymentToReceive(),
+		getPaymentReceived(),
+	])
+
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-				<Image
-					className="dark:invert"
-					src="/next.svg"
-					alt="Next.js logo"
-					width={100}
-					height={20}
-					priority
-				/>
-				<div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-					<h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-						To get started, edit the page.tsx file.
-					</h1>
-					<p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-						Looking for a starting point or more instructions? Head over to{" "}
-						<a
-							href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-							className="font-medium text-zinc-950 dark:text-zinc-50"
-						>
-							Templates
-						</a>{" "}
-						or the{" "}
-						<a
-							href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-							className="font-medium text-zinc-950 dark:text-zinc-50"
-						>
-							Learning
-						</a>{" "}
-						center.
-					</p>
-				</div>
-				<div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-					<a
-						className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Image
-							className="dark:invert"
-							src="/vercel.svg"
-							alt="Vercel logomark"
-							width={16}
-							height={16}
-						/>
-						Deploy Now
-					</a>
-					<a
-						className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Documentation
-					</a>
-				</div>
+				<DashboardCard
+					title="Lessons"
+					subtitle="Manage worked lessons"
+				>
+					<p>Total worked hours: {workedLessons}</p>
+				</DashboardCard>
+
+				<DashboardCard
+					title="Payments"
+					subtitle="Check payments received and to be received"
+				>
+					<>
+						<p>Total payments to receive: {paymentToReceive} €</p>
+						<p>Total payments received: {paymentReceived} €</p>
+					</>
+				</DashboardCard>
 			</main>
 		</div>
 	);
+}
+
+type DashboardCardProps = {
+	title: string,
+	subtitle: string,
+	children: JSX.Element,
+}
+
+function DashboardCard({
+	title,
+	subtitle,
+	children,
+}: DashboardCardProps) {
+	return <>
+		<Card className="w-full max-w-sm">
+			<CardHeader>
+				<CardTitle>{title}</CardTitle>
+				<CardDescription>{subtitle}</CardDescription>
+			</CardHeader>
+			<CardContent>
+				{children}
+			</CardContent>
+		</Card>
+	</>
 }
