0ff9fcf67799e036428b9debff06f7ebc0799891
Tocco17 <fede.ruocco@gmail.com>
2025-12-26 19:06:02 +0100

    operations

--- bushido.segna-ore.next/app/_utils/operations/date-operations.ts
+++ bushido.segna-ore.next/app/_utils/operations/date-operations.ts
@@ -1,1 +1,48 @@
+import { toDigitArray } from "./math-operations"
+
 export const getDate = ({day, month, year}: CustomDateObject) => `${day}/${month}/${year}`
+
+export const getDayFromNumber: (value: number) => Day = (value) => {
+	if(value <= 0 || value > 31)
+		throw new Error('The value must be positive and cannot be greater than 31.')
+
+	if(value % 1 != 0)
+		throw new Error("The value must be an integer.")
+
+	const digits = toDigitArray(value, 2)
+
+	const day = `${digits[0]}${digits[1]}` as Day
+	return day
+}
+
+export const getMonthFromNumber: (value: number) => Month = (value) => {
+	if(value <= 0 || value > 12)
+		throw new Error('The value must be positive and cannot be greater than 12.')
+
+	if(value % 1 != 0)
+		throw new Error("The value must be an integer.")
+
+	const digits = toDigitArray(value, 2)
+
+	const month = `${digits[0]}${digits[1]}` as Month
+	return month
+}
+
+export const getYearFromNumber: (value: number) => Year = (value) => {
+	if(value <= 0 && value > 10000)
+		throw new Error('The value must be positive and cannot be greater than 9999.')
+
+	if(value % 1 != 0)
+		throw new Error("The value must be an integer.")
+
+	let digits = toDigitArray(value, 4)
+
+	const year = `${digits[0]}${digits[1]}${digits[2]}${digits[3]}` as Year
+	return year
+}
+
+export const getCustomDateObject = (day: number, month: number, year: number) => ({
+	day: getDayFromNumber(day),
+	month: getMonthFromNumber(month),
+	year: getYearFromNumber(year),
+} as CustomDateObject)
--- bushido.segna-ore.next/app/_utils/operations/math-operations.ts
+++ bushido.segna-ore.next/app/_utils/operations/math-operations.ts
@@ -7,13 +7,20 @@ export const toDigit: (value: number) => Digit = (value) => {
 
 export const getStringAsNumber = (value: `${number}`) => Number(value)
 
-export const toDigitArray: (value: number) => Digit[] = (value) => {
+export const toDigitArray: (value: number, numberOfDigits?: number) => Digit[] = (value, numberOfDigits) => {
 	const valueString = `${value}`
 	if(valueString.includes('.'))
 		throw new Error('Only integer numbers are accepted')
 	
 	const valueStringArray = valueString.split('')
-	const digitArray = valueStringArray
+	let digitArray = valueStringArray
 		.map(current => toDigit(Number(current)))
+	
+	if(!numberOfDigits)
+		return digitArray
+		
+	while(digitArray.length < numberOfDigits)
+		digitArray = [0, ...digitArray]
+
 	return digitArray
 }
