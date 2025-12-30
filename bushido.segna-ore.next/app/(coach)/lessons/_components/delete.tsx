a89615ecc7e964534afd56cf9990d68f260e5719
Tocco17 <fede.ruocco@gmail.com>
2025-12-30 13:02:22 +0100

    clean

--- bushido.segna-ore.next/app/(coach)/lessons/_components/delete.tsx
+++ bushido.segna-ore.next/app/(coach)/lessons/_components/delete.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Lesson } from "@/app/_utils/entities/Lesson"
-import { getLessonDate } from "@/app/_utils/operations/entites/lessons-operations";
+import { getLessonDateString } from "@/app/_utils/operations/entites/lessons-operations";
 import {
 	AlertDialog,
 	AlertDialogAction,
@@ -34,7 +34,7 @@ export const DeleteLesson = ({
 				<AlertDialogHeader>
 					<AlertDialogTitle>Are you sure?</AlertDialogTitle>
 					<AlertDialogDescription>
-						You are about to delete the lesson you have performed on date <strong>{getLessonDate(lesson)}</strong>.
+						You are about to delete the lesson you have performed on date <strong>{getLessonDateString(lesson)}</strong>.
 						This action cannot be undone.
 						You will need to add a new lesson if deleting this was a mistake.
 					</AlertDialogDescription>
@@ -45,7 +45,7 @@ export const DeleteLesson = ({
 						onClick={() => toast.success(
 							"Delete complete",
 							{
-								description: `Lesson on date ${getLessonDate(lesson)} has been succesfully deleted.`
+								description: `Lesson on date ${getLessonDateString(lesson)} has been succesfully deleted.`
 							})}
 					>
 						Continue
