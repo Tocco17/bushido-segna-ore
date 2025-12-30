090f0ccb8551ccbe3a2bc913dc8f37120fbc5731
Tocco17 <fede.ruocco@gmail.com>
2025-12-30 13:01:38 +0100

    edit lessons date operations

--- bushido.segna-ore.next/app/_utils/operations/entites/lessons-operations.ts
+++ bushido.segna-ore.next/app/_utils/operations/entites/lessons-operations.ts
@@ -1,3 +1,5 @@
 import { Lesson } from "../../entities/Lesson";
 
-export const getLessonDate = ({day, month, year}: Lesson) => `${day}/${month}/${year}`
+export const getLessonDateString = ({day, month, year}: Lesson) => `${day}/${month}/${year}`
+
+export const getLessonDate = ({day, month, year}: Lesson) => new Date(parseInt(year), parseInt(month) - 1, parseInt(day))
